fb8450688bf194811d475e17a1ae262a2c4b5391
ajmal faris <ajmalfaris.me@gmail.com>
2025-08-20 00:46:58 +0530

    fix(html): add suppressHydrationWarning to html and body tags to prevent hydration mismatch warnings in Next.js

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body
+      <body suppressHydrationWarning
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <ThemeProvider
