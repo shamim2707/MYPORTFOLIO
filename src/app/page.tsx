3641980c36f61081af7199dff94aeec4a40b408e
ajmal faris <ajmalfaris.me@gmail.com>
2025-08-21 21:10:26 +0530

    feat(home): integrate Grid section into Home page layout
    - Imported Grid component from components/Grid
    - Added Grid section below Hero2 for displaying bento-style content
    - Ensured consistent layout with flex column and centered alignment

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero1 from "@/components/Hero/HeroV1";
 import Hero2 from "@/components/Hero/HeroV2";
+import Grid from "@/components/Grid"
 
 export default function Home() {
   return (
     <main className="relative bg-black text-white flex justify-center items-center flex-col overflow-hidden mx-auto select-none">
       <Hero2 />
+      <Grid/>
     </main>
   );
 }
