e54e72bc926c3d4bc7acdc238bdcd57bbd118629
ajmal faris <ajmalfaris.me@gmail.com>
2025-08-22 23:46:43 +0530

    feat: add "Check Out More Projects" button with animated gradient overlay at bottom of Projects section

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,16 +1,16 @@
 "use client";
 
 import { FaLocationArrow } from "react-icons/fa6";
+import { IoIosArrowForward } from "react-icons/io";
 
 import { projects } from "@/data";
 import { PinContainer } from "./ui/Pin";
 
 const Projects = () => {
   return (
-    <div className="flex justify-center items-center flex-col z-20 p-5">
+    <div className="flex justify-center items-center flex-col z-20 p-5 pb-15">
       <h1 className="text-6xl font-semibold">
-        My{" "}
-        <span className="text-purple animate-shine">Projects</span>
+        My <span className="text-purple animate-shine">Projects</span>
       </h1>
       <div className="grid grid-cols-3 items-center justify-center">
         {projects.map((item) => (
@@ -18,20 +18,15 @@ const Projects = () => {
             className="lg:min-h-[28rem] h-[25rem] flex items-center justify-center sm:w-96 w-[90vw]"
             key={item.id}
           >
-            <PinContainer
-              title={item.link}
-              href= {item.link}
-            >
+            <PinContainer title={item.link} href={item.link}>
               <div className="relative flex items-center justify-center sm:w-[28vw] w-[90vw] overflow-hidden h-[20vh] lg:h-[30vh] mb-10 rounded-3xl bg-blue-700 bg-gradient-to-t from-blue-700/38 to-black">
-                <div
-                  className="relative w-full overflow-hidden h-full" 
-                >
+                <div className="relative w-full overflow-hidden h-full">
                   <img src="/bg.png" alt="bgimg" />
                   <img
-                  src={item.img}
-                  alt="cover"
-                  className="z-10 absolute -bottom-16"
-                />
+                    src={item.img}
+                    alt="cover"
+                    className="z-10 absolute -bottom-16"
+                  />
                 </div>
               </div>
 
@@ -57,7 +52,8 @@ const Projects = () => {
                       style={{
                         clipPath: `polygon(25% 5%, 75% 5%, 100% 50%, 75% 95%, 25% 95%, 0% 50%`,
                         transform: `translateX(-${5 * index + 2}px)`,
-                      }}>
+                      }}
+                    >
                       <img src={icon} alt="icon5" className="p-2" />
                     </div>
                   ))}
@@ -73,6 +69,17 @@ const Projects = () => {
             </PinContainer>
           </div>
         ))}
+      </div>
+      <div className="w-[100vw] h-[200px] absolute z-50 bottom-0 bg-gradient-to-t from-blue-700/50 via-black/70 to-transparent flex justify-center items-end pb-8">
+        <button
+          className="relative inline-flex h-12 xl:h-14 overflow-hidden rounded-full p-[1px] focus:outline-none hover:text-blue-500 hover:animate-pulse"
+        >
+          <span className="absolute inset-[-1000%] animate-[spin_3s_linear_infinite] bg-[conic-gradient(from_90deg_at_50%_50%,#0000ff_0%,#fff_50%,#0000ff_100%)]" />
+          <span
+            className={`relative inline-flex h-full w-full items-center justify-center rounded-full bg-black px-4 pr-3 py-1 text-sm font-medium backdrop-blur-3xl gap-2 xl:gap-4 xl:hover:gap-8 hover:gap-6 transform duration-300 transition-all ease-in-out`}>
+              Check Out More Projects <IoIosArrowForward className="text-2xl" />
+          </span>
+        </button>
       </div>
     </div>
   );
