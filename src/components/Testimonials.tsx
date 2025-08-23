77c1cad14aa2dc5c5c923b38724adc8b7f7093f1
ajmal faris <ajmalfaris.me@gmail.com>
2025-08-23 14:37:16 +0530

    feat: add Testimonials section with InfiniteMovingCards and InfiniteFlowCard
    - new section with animated heading and responsive layout
    - integrated InfiniteMovingCards to display testimonials with smooth horizontal scroll
    - added InfiniteFlowCard to showcase company logos with continuous flow
    - configured both components for customizable direction and speed

---
+++ src/components/Testimonials.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import React from "react";
+
+import { companies, testimonials } from "@/data";
+import { InfiniteMovingCards } from "./ui/InfiniteCards";
+import { InfiniteFlowCard } from "./ui/FlowCards";
+
+const Testimonials = () => {
+  return (
+    <section
+      id="testimonials"
+      className="py-10 z-10 flex flex-col gap-10 justify-center items-center"
+    >
+      <h1 className="text-6xl font-semibold">
+        What <span className="text-purple animate-shine">People Say</span>
+      </h1>
+
+      <div className="flex flex-col items-center max-lg:mt-10">
+        <div className="rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+          />
+        </div>
+      </div>
+      <div className="flex flex-wrap items-center justify-center gap-4 md:gap-16">
+        <InfiniteFlowCard items={companies} direction="left" speed="slow" />
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
